f883d4a51f949d5187761584b13ad8d2dd186016
asharonbaltazar <asharonbaltazar@outlook.com>
2020-08-07 13:46:55 -0500

    Initial commit

---
+++ src/Components/AddTransaction.js
@@ -0,0 +1,57 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    if (amount === "" || text === "") return e.preventDefault();
+
+    const transaction = {
+      id: Math.floor(Math.random() * 100000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(transaction);
+
+    setText("");
+    setAmount("");
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h3 className="h3">Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            placeholder="Enter text..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            placeholder="Enter amount..."
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
---
+++ src/Components/TransactionList.js
@@ -0,0 +1,61 @@
+import React, { useContext, useState } from "react";
+import Transaction from "./Transaction";
+import Search from "./Search";
+import { GlobalContext } from "../context/GlobalState";
+
+const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+  const [search, showSearch] = useState(false);
+  const [query, setQuery] = useState("");
+
+  let filtered = (query) => {
+    query = Array.from(query).reduce(
+      (a, v, i) => `${a}[^${query.substr(i)}]*?${v}`,
+      ""
+    );
+    const regex = RegExp(query);
+    let result = transactions.filter((transaction) =>
+      transaction.text.match(regex)
+    );
+    return result;
+  };
+
+  const searchResults = () => {
+    if (search && query.length > 0) {
+      return filtered.map((transaction) => (
+        <Transaction key={transaction.id} transaction={transaction} />
+      ));
+    } else if (!search) {
+      return transactions.map((transaction) => (
+        <Transaction key={transaction.id} transaction={transaction} />
+      ));
+    }
+  };
+
+  return (
+    <>
+      <div className="history-search">
+        <h3>History</h3>
+        <Search
+          search={search}
+          query={query}
+          setQuery={setQuery}
+          filteredExpenses={filtered}
+        />
+        <button className="search-button" onClick={() => showSearch(!search)}>
+          {search ? (
+            <i className="fas fa-times"></i>
+          ) : (
+            <i className="fas fa-search"></i>
+          )}
+        </button>
+      </div>
+
+      <ul className="list">
+        {transactions.length === 0 ? "You have no expenses." : searchResults()}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
