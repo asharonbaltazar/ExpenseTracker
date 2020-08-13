82532b71bcefc1eaab6bffc3f539a4b40cef4e0d
asharonbaltazar <asharonbaltazar@outlook.com>
2020-08-13 15:59:17 -0500

    Started to work on showing the edit and delete button

--- client/src/Components/Transaction.js
+++ client/src/Components/Transaction.js
@@ -1,25 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { numberWithCommas } from "../utils/format";
-
 import { GlobalContext } from "../context/GlobalState";
 
 const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
+  const [show, setShow] = useState(false);
 
   const sign = transaction.amount < 0 ? "-" : "+";
 
+  // Individual button colors
+  const remove = {
+    backgroundColor: "#e74c3c",
+  };
+  const pen = {
+    backgroundColor: "#9c88ff",
+  };
+
   return (
-    <li className={transaction.amount < 0 ? "minus" : "plus"}>
-      {transaction.text}{" "}
+    <li
+      className={transaction.amount < 0 ? "minus" : "plus"}
+      onClick={(e) => {
+        setShow(!show);
+        console.log(e.target);
+      }}
+    >
+      {transaction.text}
       <span>
         {sign}${numberWithCommas(Math.abs(transaction.amount))}
       </span>
-      <button
-        className="delete-btn"
-        onClick={() => deleteTransaction(transaction._id)}
-      >
-        <i className="fas fa-times"></i>
-      </button>
+      <div className="list-buttons">
+        <button style={remove}>
+          <i className="fas fa-times"></i>
+        </button>
+        <button style={pen}>
+          <i className="fas fa-pen"></i>
+        </button>
+      </div>
     </li>
   );
 };
